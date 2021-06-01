14574eaf3c5491711695490c236fb7fbea080586
Kathryn Senack <katbrooke0219@gmail.com>
2021-06-01 19:32:44 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
 
-display_rainbow ['red', 'organge', 'yellow', 'green', 'blue', 'indigo', 'violet']
+display_rainbow ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 #display_rainbow(Colors) ('R: red', 'O: orange', 'Y: yellow', 'G:green', 'B:blue', 'I:indigo', 'V:violet')
